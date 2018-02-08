9d376b9011477fef46792a02df8bcaf698035bb6
pablo <ppsammartino@gmail.com>
2018-02-08 16:24:32 -0300

    Refactored Views. They all inherit from BaseEventView now.

--- content/views.py
+++ content/views.py
@@ -1,11 +1,12 @@
 import os
 
+import re
 from django.apps import apps
 from django.conf import settings
 from django.contrib import messages
 from django.core.cache import cache
-from django.http import HttpResponse, Http404
-from django.shortcuts import render
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.shortcuts import render, redirect
 from django.template import loader
 from django.urls import reverse
 from django.views.generic import DetailView, TemplateView, RedirectView
@@ -13,50 +14,18 @@ from django.views.generic import DetailView, TemplateView, RedirectView
 from filebrowser.decorators import path_exists, get_path
 from filebrowser.sites import filebrowser_view, site as filebrowser_site
 
+from events.models import Event
 from menus.models import BigHeaderMenu, FooterMenu
 from content.models import Page, Module, Talk
 
 
-class HomeView(TemplateView):
+class BaseEventView(DetailView):
 	template_name = 'base.html'
 	model = Page
 
-	def get_footer_menus(self, event_slug):
-		return FooterMenu.objects.filter(event__slug=event_slug).order_by('order')
-
-	def get_context_data(self, **kwargs):
-		context = super(HomeView, self).get_context_data(**kwargs)
-		event_slug = self.request.META['HTTP_HOST'].split('.')[0]
-		try:
-			page = Page.objects.get(slug='', event__slug=event_slug)
-		except:
-			page = Page.objects.filter(event__slug=event_slug).first()
-		if not page.event.slug == event_slug:
-			raise Http404("Requested page doesn't exist")
-		header_menus = BigHeaderMenu.objects.filter(event__slug=event_slug).order_by('order')
-		footer_menus = self.get_footer_menus(event_slug)
-
-		modules = Module.objects.filter(modules_in_page__page=page, event__slug=event_slug).order_by('modules_in_page__order')
-		context.update({
-			'pages': header_menus,
-			'title': page.title,
-			'footer_menus': footer_menus,
-			'page' : page,
-			'modules' : modules
-		})
-		return context
-
-
-class PageView(DetailView):
-	template_name = "base.html"
-	model = Page
-	context_object_name = "page"
-
-	def get_menus(self, event_slug):
-		return BigHeaderMenu.objects.filter(event__slug=event_slug).order_by('order')
-
-	def get_footer_menus(self, event_slug):
-		return FooterMenu.objects.filter(event__slug=event_slug).order_by('order')
+	def __init__(self):
+		super(BaseEventView, self).__init__()
+		self.page = None
 
 	def get_object(self, queryset=None):
 		if queryset is None:
@@ -68,23 +37,56 @@ class PageView(DetailView):
 		obj = queryset.filter(event__slug=event_slug, slug=slug).get()
 		return obj
 
+	def get_menus(self, event_slug):
+		return BigHeaderMenu.objects.filter(event__slug=event_slug).order_by('order')
+
+	def get_footer_menus(self, event_slug):
+		return FooterMenu.objects.filter(event__slug=event_slug).order_by('order')
+
+	def dispatch(self, request, *args, **kwargs):
+		self.event_slug = re.match(r'(www.)?(\w+)\.(' + re.escape(settings.SITE_HOST) + ')', self.request.META['HTTP_HOST']).group(2)
+		if not self.event_slug:
+			last_event_slug = Event.objects.all().last().slug
+			return redirect('http://{}.{}'.format(last_event_slug, settings.SITE_HOST))
+
+		return super(BaseEventView, self).dispatch(request, *args, **kwargs)
+
 	def get_context_data(self, **kwargs):
-		context = super(PageView, self).get_context_data(**kwargs)
-		event_slug = self.request.META['HTTP_HOST'].split('.')[0]
-		page = kwargs['object']
-		if not page.event.slug == event_slug:
+		context = super(BaseEventView, self).get_context_data(**kwargs)
+		if not self.page.event.slug == self.event_slug:
 			raise Http404("Requested page doesn't exist")
-		header_menus = self.get_menus(event_slug)
-		footer_menus = self.get_footer_menus(event_slug)
-		modules = Module.objects.filter(modules_in_page__page=page, event__slug=event_slug).order_by('modules_in_page__order')
+		header_menus = BigHeaderMenu.objects.filter(event__slug=self.event_slug).order_by('order')
+		footer_menus = self.get_footer_menus(self.event_slug)
+
+		modules = Module.objects.filter(modules_in_page__page=self.page, event__slug=self.event_slug).order_by('modules_in_page__order')
 		context.update({
-			'pages': header_menus,
-			'footer_menus' : footer_menus,
-			'title': page.title,
-			'page' : page,
+			'header_menus': header_menus,
+			'title': self.page.title,
+			'footer_menus': footer_menus,
+			'page' : self.page,
 			'modules' : modules
 		})
 		return context
+
+
+class HomeView(BaseEventView):
+
+	def get_object(self, queryset=None):
+		try:
+			self.page = Page.objects.get(slug='', event__slug=self.event_slug)
+		except:
+			self.page = Page.objects.filter(event__slug=self.event_slug).first()
+		return self.page
+
+
+class PageView(BaseEventView):
+
+	context_object_name = "page"
+
+	def get_context_data(self, **kwargs):
+		self.page = kwargs['object']
+
+		return super(PageView, self).get_context_data(**kwargs)
 
 
 class TalkView(PageView):
