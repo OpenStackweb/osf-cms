f27a6b8516a9e3e7550a936c7cfda1b24c0dd4ad
pablo <ppsammartino@gmail.com>
2018-02-14 16:22:51 -0300

    Fixed: Youtube links are now parsed automatically into iframe.

--- content/templatetags/custom_tags.py
+++ content/templatetags/custom_tags.py
@@ -1,3 +1,4 @@
+import re
 from django import template
 from django.shortcuts import get_object_or_404
 from django.template import VariableDoesNotExist, Node
@@ -11,105 +12,115 @@ register = template.Library()
 
 @register.simple_tag(name='add_active')
 def add_active(request, name, by_path=False):
-	""" Return the string 'active' current request.path is same as name
+    """ Return the string 'active' current request.path is same as name
 
-	Keyword aruguments:
-	request  -- Django request object
-	name     -- name of the url or the actual path
-	by_path  -- True if name contains a url instead of url name
-	"""
-	if by_path:
-		path = name
-	else:
-		path = reverse('page', kwargs={'slug': name})
+    Keyword aruguments:
+    request  -- Django request object
+    name     -- name of the url or the actual path
+    by_path  -- True if name contains a url instead of url name
+    """
+    if by_path:
+        path = name
+    else:
+        path = reverse('page', kwargs={'slug': name})
+    
+    # Adjustment for Home page
+    if path == '/%2F':
+        path = '/'
+    
+    if request.path == path:
+        return ' active '
+    
+    return ''
 
-	# Adjustment for Home page
-	if path == '/%2F':
-		path = '/'
-
-	if request.path == path:
-		return ' active '
-
-	return ''
 
 @register.simple_tag(name='add_horizontal_separator')
 def add_horizontal_separator(list_style):
-	""" Return the string 'active' current request.path is same as name
+    """ Return the string 'active' current request.path is same as name
 
-	Keyword aruguments:
-	request  -- Django request object
-	name     -- name of the url or the actual path
-	by_path  -- True if name contains a url instead of url name
-	"""
-	if list_style == 'HORIZONTALSEP' or list_style == 'VERTICALHORIZONTAL':
-		return ' top-side '
-
-	return ''\
-
+    Keyword aruguments:
+    request  -- Django request object
+    name     -- name of the url or the actual path
+    by_path  -- True if name contains a url instead of url name
+    """
+    if list_style == 'HORIZONTALSEP' or list_style == 'VERTICALHORIZONTAL':
+        return ' top-side '
+    
+    return ''
 
 @register.simple_tag(name='add_vertical_separator')
-def add_vertical_separator(is_last ,list_style):
-	""" Return the string 'active' current request.path is same as name
+def add_vertical_separator(is_last, list_style):
+    """ Return the string 'active' current request.path is same as name
 
-	Keyword aruguments:
-	request  -- Django request object
-	name     -- name of the url or the actual path
-	by_path  -- True if name contains a url instead of url name
-	"""
-	if (list_style == 'VERTICALSEP' or list_style == 'VERTICALHORIZONTAL') and not is_last:
-		return ' left-side '
-
-	return ''
+    Keyword aruguments:
+    request  -- Django request object
+    name     -- name of the url or the actual path
+    by_path  -- True if name contains a url instead of url name
+    """
+    if (list_style == 'VERTICALSEP' or list_style == 'VERTICALHORIZONTAL') and not is_last:
+        return ' left-side '
+    
+    return ''
 
 
 @register.simple_tag(name='set_col_size')
 def set_col_size(item_count, col_type):
-	if item_count == 5:
-		col_size = '5ths'
-	else:
-		col_size = int(12 / int(item_count))
-
-	return 'col-{}-{}'.format(col_type, str(col_size))\
-
+    if item_count == 5:
+        col_size = '5ths'
+    else:
+        col_size = int(12 / int(item_count))
+    
+    return 'col-{}-{}'.format(col_type, str(col_size))
 
 @register.simple_tag(name='set_justify')
 def set_justify(justify):
-	if justify == 'CENTER':
-		return ' align-center '
-	elif justify == 'RIGHT':
-		return ' align-right '
-	return ' align-left '
+    if justify == 'CENTER':
+        return ' align-center '
+    elif justify == 'RIGHT':
+        return ' align-right '
+    return ' align-left '
 
 
 @register.simple_tag(name='get_next_talk')
 def get_next_talk(talk):
-	order = talk.order
-	next = Talk.objects.filter(order=order + 1).first()
-	if next:
-		return next.get_absolute_url()
-	return False
+    order = talk.order
+    next = Talk.objects.filter(order=order + 1).first()
+    if next:
+        return next.get_absolute_url()
+    return False
+
 
 @register.simple_tag(name='get_prev_talk')
 def get_prev_talk(talk):
-	order = talk.order
-	prev = Talk.objects.filter(order=order - 1).first()
-	if prev:
-		return prev.get_absolute_url()
-	return False
+    order = talk.order
+    prev = Talk.objects.filter(order=order - 1).first()
+    if prev:
+        return prev.get_absolute_url()
+    return False
+
 
 @register.simple_tag(name='get_events')
 def get_events():
-	return Event.objects.all()
+    return Event.objects.all()
+
 
 @register.simple_tag(name='get_current_event')
 def get_current_event(request):
-	slug = request.META['HTTP_HOST'].split('.')[0]
-	event = get_object_or_404(Event, slug=slug)
-	return event
+    slug = request.META['HTTP_HOST'].split('.')[0]
+    event = get_object_or_404(Event, slug=slug)
+    return event
+
 
 @register.simple_tag(name='get_videos_row')
 def get_videos_row(per_row, videos):
-	rows = int(videos) // int(per_row)
-	if int(videos) % int(per_row):
-		rows += 1
-	return rows
+    rows = int(videos) // int(per_row)
+    if int(videos) % int(per_row):
+        rows += 1
+    return rows
+
+
+@register.simple_tag(name='parse_youtube_link')
+def parse_youtube_link(link):
+    video_id = re.match(r'(https:\/\/)?(www.)?(youtube.com\/watch\?v=)(\w+)', link).group(4)
+    formatted_link = 'https://www.youtube.com/embed/{}?rel=0&showinfo=0'.format(video_id)
+    return formatted_link
