b9579c348c76e20091b2c394e8a5d268d7abba4b
pablo <ppsammartino@gmail.com>
2018-02-15 14:01:44 -0300

    Added: help_text to Logo field (Event)

--- events/models.py
+++ events/models.py
@@ -9,7 +9,7 @@ from events.cloneevent import CloneViewSet
 class Event(models.Model):
     title = models.CharField(max_length=50, blank=False)
     slug = models.SlugField(unique=True)
-    logo = FileBrowseField(max_length=200, directory='logos', format='Icon', blank=True, null=True)
+    logo = FileBrowseField(max_length=200, directory='logos', format='Icon', blank=True, null=True, help_text = "Will be copied from the original event if you use the clone feature.")
     start_date = models.DateField(blank=False)
     public = models.BooleanField(default=True)
     custom_css = models.TextField(blank=True, null=True, verbose_name='Custom CSS')
